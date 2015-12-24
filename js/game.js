9ec49c9c82e3e5836c1716f3f03667540b062463
OrionShah <dronshv@yandex.ru>
2015-12-25 00:08:42 +0300

    Переход на CreateJS

--- js/game.js
+++ js/game.js
@@ -1,98 +1,48 @@
-timer_for_fps = 0;
+var res_paths = {
+    "manifest": [
+        {id: "char", src:"Person.png"}
+    ]
+};
 
-var fps_model = Backbone.Model.extend({
-    id: 'timer',
-    fps: 0,
-    screens: 0,
-    calc: function () {
-        this.fps = this.screens / 0.1;
-        this.screens = 0;
-        // console.log(this.fps);
-    }
-});
-var GUI_View = Backbone.KonvaView.extend({
-    fps_timer: 123213,
-    initialize: function (params) {
-        this.layer = params.layer;
-        this.fps_timer = params.fps_timer;
-        console.log(this.fps_timer);
-    },
-    events: {
-    	'keypress': 'keyAction',
-    	'click': "keyAction"
-    },
-    keyAction: function (e)  {
-    	console.log(e);
-    },
-    g: function () {
-        var group = new Konva.Group();
-        var imageObj = new Image();
-        var player;
-        var self = this;
-        console.log(self.fps_timer);
-        imageObj.onload = function() {
-            player = new Konva.Image({
-                id: "player",
-                x: 100,
-                y: 100,
-                image: imageObj,
-                width: 300,
-                height: 300
-            });
-        	group.add(player);
+function init () {
+    load_res();
+    createjs.Ticker.setFPS(60);
+    var stage = new createjs.Stage('canvas_id');
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill('DeepSkyBlue').drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    circle.addEventListener('click', function (e) {
+        console.log(e);
+    });
+    stage.addChild(circle);
+
+    var text_fps = new createjs.Text(createjs.Ticker.getMeasuredFPS(), "20pt Arial", "#000");
+    text_fps.x = 15;
+    stage.addChild(text_fps);
+    console.log(createjs.EaselJS.version);
+    createjs.Ticker.addEventListener("tick", function () {
+        text_fps.set({text: createjs.Ticker.getFPS()});
+        circle.x += 10;
+        if (circle.x > stage.canvas.width) {
+            circle.x = 0;
         }
-        imageObj.src = "Person.png";
-        
-        var rect = new Konva.Rect({
-          	x : 100,
-          	y : 100,
-          	width : 50,
-          	height : 50,
-          	fill : 'green',
-          	id : 'rect'
-        });
-        var fps_text = new Konva.Text({
-            x: 20,
-            y: 20,
-            text: 'sadsf',
-            fontSize: 30,
-            fontFamily: 'Calibri',
-            fill: 'green'
-        });
-        group.add(fps_text);
-        // group.add(rect);
-        // console.log(group);
-        return group;
-    },
-    render: function () {
-        console.log(this);
-        this.layer.add(this.g);
-        this.layer.draw();
-    },
-});
+        stage.update();
+    });
 
+    // stage.update();
+}
 
-var stage = new Konva.Stage({
-    container: 'game_view',
-    // width: 1000,
-    // height: 700,
-    width:  window.innerWidth-20,
-    height: window.innerHeight-20
-});
+function load_res () {
+    var res = new createjs.LoadQueue();
+    res.on("progress", function() {
+        console.log("Progress:", res.progress);
+    });
+    res.on("complete", function (e) {
 
-var layer = new Konva.Layer();
-stage.add(layer);
-
-fps = new fps_model();
-gui = new GUI_View({layer: layer, fps_timer: fps});
-gui.render();
-setInterval(function () {
-    // gui.render();
-    // fps.screens++;
-    // console.log('render');
-}, 20);
-
-
-setInterval(function () {
-    fps.calc();
-}, 100);
+        console.log('all loaded');
+        console.log(res);
+        console.log(e.item);
+    });
+    res.loadManifest(res_paths);
+}
--- js/system_helpers.js
+++ js/system_helpers.js
@@ -1,8 +1,6 @@
 var $ = require('jquery');
 var _ = require('underscore');
-var Backbone = require('backbone');
-var Konva = require('konva');
-// $(document).ready(function() {
-// 	$('body').append("<canvas id='canvas_id' width= " + ($('body').width()-10) 
-// 		+ " height=" + ($(document).height()-20) + "></canvas>");
-// });
+$(document).ready(function() {
+	$('body').append("<canvas id='canvas_id' width= " + ($('body').width()-10) 
+		+ " height=" + ($(document).height()-20) + "></canvas>");
+});
